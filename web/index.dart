d4320fc05dfab60a2b4461d798b300be925e23d7
张利杰 <mumuqaz@qq.com>
2015-10-03 20:13:54 +0800

    exercise_1
    第一次作业：实现从1加到100的加法器

--- web/index.dart
+++ web/index.dart
@@ -1,29 +1,21 @@
 import 'dart:html';
-import 'dart:math' as math;
-void main() {
-  querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+void main()
+{
+  querySelector("#btn1")
+    ..onClick.listen(Add);
+  querySelector("#btn2")
+    ..onClick.listen(Clear);
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
+Add(MouseEvent event) {
+  var s = 0;
+  var i;
+  for (i = 1;i <= 100;i++) {
+    s = s + i;
   }
-  querySelector('#sample_text_id').text = buffer.toString();
+  querySelector('#txt1').value=s.toString();
 }
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
-  };
-  var random = new math.Random();
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
+
+Clear(MouseEvent event) {
+  querySelector('#txt1').value='';
 }
