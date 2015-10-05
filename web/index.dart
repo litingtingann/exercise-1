3d894b9502a1aaa48b6be251811cca5b0047f6fd
田彬 <1280039028@qq.com>
2015-10-05 14:17:41 +0800

    exercise_1
    第一次作业

--- web/index.dart
+++ web/index.dart
@@ -1,29 +1,17 @@
 import 'dart:html';
-import 'dart:math' as math;
+
 void main() {
   querySelector('#sample_text_id')
     ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+    ..onClick.listen(add);
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
+
+void add(MouseEvent event){
+  var sum=0;
+  for (var i = 0; i < 100; i++) {
+    sum=sum+i;
   }
-  querySelector('#sample_text_id').text = buffer.toString();
-}
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
-  };
-  var random = new math.Random();
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
+  print('1+2+3+бн+100=');
+  querySelector('#sample_teat_id').text = sum.toString();
 }
